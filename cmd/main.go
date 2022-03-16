4008c0d4069b9fc8040474ae2bf41921d51b3266
martynocchio <martynocchio@mail.ru>
2022-03-16 22:51:10 +0300

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	"restAPI"
 	"restAPI/pkg/handler"
+	"restAPI/pkg/repository"
+	"restAPI/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(restAPI.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server, %s", err.Error())
