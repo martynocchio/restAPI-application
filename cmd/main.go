246387c2e65d93117c721c6db96e1945f0433049
martynocchio <martynocchio@mail.ru>
2022-03-26 02:57:49 +0300

    implemented shutdown method

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
+	"os/signal"
 	"restAPI"
 	"restAPI/pkg/handler"
 	"restAPI/pkg/repository"
 	"restAPI/pkg/service"
+	"syscall"
 )
 
 func main() {
@@ -39,8 +42,24 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(restAPI.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("error occured while running http server, %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server, %s", err.Error())
+		}
+	}()
+
+	logrus.Printf("restAPI started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Printf("restAPI shutting down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
 	}
 }
 
